184ef13d3ad53204a97e8a66114cee8b5a0d75e4
Ryan Hanley <camelCaseGuy@users.noreply.github.com>
2022-09-19 18:08:20 -0500

    Initial classes

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log('welcome');
+
+class Node {
+  constructor() {
+
+  }
+  get value () {
+    return this.value;
+  }
+  get leftNode () {
+    return this.leftNode;
+  }
+  get rightNode () {
+    return this.rightNode;
+  }
+}
+
+class Tree {
+  constructor(arr) {
+
+  }
+  get root () {
+    return this.root;
+  }
+}
+
+function buildTree(arr) {
+  const myTree = new Tree(arr);
+}
